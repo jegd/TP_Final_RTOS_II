e2b7b6345e8aee1d37f6fba5eeea213caee4e946
Jesus Gonzales <jesus.e_97@hotmail.com>
2023-12-09 02:08:33 -0500

    Funcionando_driver_comentado

--- RTOSII_tpfinal/src2/app/src/driver_i2c1.c
+++ RTOSII_tpfinal/src2/app/src/driver_i2c1.c
@@ -1,8 +1,10 @@
-/*
- * driver_i2c1.c
+/** @file driver_i2c1.c
+ *  @Created on: Dic 08, 2023
+ *  @Authors:  Anthony  Maisincho              N°SIU e2011
+ *             Jesus    Gonzales               N°SIU e2006
  *
- *  Created on: Dec 8, 2023
- *      Author: Jesus
+ *  @brief Driver de manejo de I2C
+ *
  */
 
 #include "driver_i2c1.h"
@@ -17,20 +19,12 @@
 #include "app.h"
 #include "driver.h"
 #include "supporting_Functions.h"
-//Defines de la HAL
 
+//Defines de la HAL
 #define HAL_I2C_INIT(PORT,I2C_GPIO_DEF) HAL_GPIO_Init(PORT,I2C_GPIO_DEF)
 #define I2C_HANDLE_DEF 	I2C_HandleTypeDef
 #define I2C_HAL_HANDLE_  (&hi2c1)  //por ver
 #define I2C_BUFFER_SIZE (32)
-
-#define scan_address_length     30
-#define trial_scan               3
-#define timeout_scan            10
-#define timeout_transmit       100
-#define max_slave              128
-#define desplazamiento_7_bits    1
-#define one_byte                 1
 
 //Defines de la OSAL
 #define ENTER_CRITICAL_()	taskENTER_CRITICAL()
@@ -48,19 +42,45 @@ extern I2C_HandleTypeDef hi2c1;
 static i2c_tx_t i2c_tx;
 //static 	I2C_GPIO_DEF i2c_gpio;
 bool flag = 1;
-
+/*!
+ * @brief Callback del error I2C.
+ *
+ * @param[I2C_HandleTypeDef] Handle del I2C
+ *
+ * @return Función del tipo void.
+ */
 void Hal_i2c_errorCallback(I2C_HandleTypeDef *hi2c) {
 	i2c_error_callback();
 }
-
+/*!
+ * @brief Callback de finalización de transmisión I2C
+ *
+ * @param[I2C_HandleTypeDef] Handle del I2C
+ *
+ * @return Función del tipo void.
+ */
 void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
 	flag = true;
 }
-
+/*!
+ * @brief Función para declarar por el usuario y manejar errores
+ *
+ * @param[void]
+ *
+ * @return Función del tipo void.
+ */
 __weak void i2c_error_callback(void) {
 	return;
-
 }
+/*!
+ * @brief Función para declarar por el usuario y manejar errores
+ *
+ * @param[uint8_t *] buffer		Puntero al buffer a transmitir
+ * @param[size_t]	size		Tamaño de lo que se quiere transmitir
+ * @param[uint16_t] 	address	Dirección I2C
+ *
+ * @return bool true si es que se envió correctamente
+ */
 bool i2c_enviar(uint8_t *buffer, size_t size, uint16_t address) {
 	if (NULL == buffer)
 		i2c_error_callback();
@@ -76,7 +96,9 @@ bool i2c_enviar(uint8_t *buffer, size_t size, uint16_t address) {
 		case HAL_ERROR:
 			i2c_error_callback();
 		case HAL_BUSY:
+			break;
 		case HAL_TIMEOUT:
+			break;
 		default:
 			i2c_error_callback();
 			break;
@@ -92,10 +114,23 @@ uint16_t scan_adress(void) {
 	uint8_t address;
 	for (address = 0; address < max_slave; address++) {
 		ENTER_CRITICAL_();
-		bool ack = HAL_I2C_IsDeviceReady(I2C_HAL_HANDLE_,
+		HAL_StatusTypeDef ack = HAL_I2C_IsDeviceReady(I2C_HAL_HANDLE_,
 				address << desplazamiento_7_bits, trial_scan, timeout_scan); //Al enviarse 8 bits siempre, y la direccion es de 7, se hace desplazamiento de 1 a la izquierda
-		if (ack == HAL_OK)
-			EXIT_CRITICAL_();
+		switch (ack) {
+		case HAL_OK:
+			break;
+		case HAL_ERROR:
+			i2c_error_callback();
+		case HAL_BUSY:
+			break;
+		case HAL_TIMEOUT:
+			break;
+		default:
+			i2c_error_callback();
+			break;
+		}
+
 	}
+	EXIT_CRITICAL_();
 	return address;
 }
